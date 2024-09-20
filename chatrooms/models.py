02306139e632f44be1438d79a93a79cec72b94e7
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-20 07:52:09 +0500

    Add Topic model

--- chatrooms/models.py
+++ chatrooms/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
