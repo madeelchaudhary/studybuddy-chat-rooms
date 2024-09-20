d4ead1d1caf4c88a36c13f502820678f2e09fdcd
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-20 08:01:47 +0500

    Add Room model

--- chatrooms/models.py
+++ chatrooms/models.py
@@ -1,10 +1,27 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=200, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    description = models.TextField(blank=True)
+
+    topic = models.ForeignKey(
+        Topic, on_delete=models.PROTECT, related_name="rooms")
+    host = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="rooms")
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.name
