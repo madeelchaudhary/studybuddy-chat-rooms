1eb93cd97db030e8949afbf5f26e6c9085ca859e
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-22 10:49:46 +0500

    Add RegisterForm

--- chatrooms/forms.py
+++ chatrooms/forms.py
@@ -1,7 +1,18 @@
+from typing import Any
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150, required=True,)
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={'autocomplete': 'password'}))
+
+
+class RegisterForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['username', 'password1', 'password2']
