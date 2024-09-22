7bbc6c82f9c71d957d22185c281065e6d9d283be
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-22 09:25:07 +0500

    Add LoginForm

---
+++ chatrooms/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150, required=True,)
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'autocomplete': 'password'}))
