30634f1e7ab0d2be416e8343596d7ad470125d3c
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-22 09:45:34 +0500

    Add login view

--- chatrooms/urls.py
+++ chatrooms/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.HomeView.as_view(), name="home"),
+    path('login/', views.LoginView.as_view(), name="login")
 ]
--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from typing import Any
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.views.generic import View, FormView
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+from chatrooms.forms import LoginForm
 
 # Create your views here.
+
+
+class HomeView(View):
+    def get(self, request: HttpRequest):
+        return HttpResponse("Homepage")
+
+
+class LoginView(FormView):
+    form_class = LoginForm
+    template_name = "chatrooms/login.html"
+    success_url = '/'
+
+    def get(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect('home')
+
+        return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form: Any) -> HttpResponse:
+        user = authenticate(request=self.request, **form.cleaned_data)
+
+        if user:
+            login(request=self.request, user=user)
+            return super().form_valid(form)
+        else:
+            messages.error(request=self.request,
+                           message="Either username or password is incorrect")
+
+        return super().form_invalid(form)
