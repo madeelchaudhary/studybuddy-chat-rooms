3b168019d344bf3b587fcffcbb1a154c170501b0
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-26 09:14:18 +0500

    Add logout view

--- chatrooms/urls.py
+++ chatrooms/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+
 from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home"),
     path('topics/', views.AllTopicsView.as_view(), name="all_topics"),
+
     path('create-room/', views.CreateRoomView.as_view(), name="create_room"),
     path('rooms/<int:pk>/edit', views.UpdateRoomView.as_view(), name="edit_room"),
 
     path('login/', views.LoginView.as_view(), name="login"),
-    path('register/', views.RegisterView.as_view(), name="register")
+    path('register/', views.RegisterView.as_view(), name="register"),
+    path('logout/', view=LogoutView.as_view(), name="logout")
 ]
