25f3803ec00a7bc313dd395a863c3b1bba417119
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-28 09:12:55 +0500

    Register Room admin

--- chatrooms/admin.py
+++ chatrooms/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from chatrooms.models import Room
+
 # Register your models here.
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['name', 'host']
+    search_fields = ['name']
+    list_filter = ['topic', 'host']
