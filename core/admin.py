76cec53ed9c041cffaaa60398d1942848dc94b81
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-28 09:26:58 +0500

    Register User admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from core.models import User
+
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['username', 'first_name', 'last_name']
+
+    list_display = ['username', 'first_name',
+                    'last_name', 'is_active', 'is_staff', 'last_login']
+    list_filter = ['is_active', 'is_staff']
