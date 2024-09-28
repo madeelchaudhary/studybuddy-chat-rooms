2fac96464ecef5dd2fc6b143de24e88cad21966a
madeelchaudhry <madeelchaudhry@gmail.com>
2024-09-28 09:15:20 +0500

    Register Topic admin

--- chatrooms/admin.py
+++ chatrooms/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from chatrooms.models import Room
+from chatrooms.models import Room, Topic
 
 # Register your models here.
 
@@ -10,3 +10,8 @@ class RoomAdmin(admin.ModelAdmin):
     list_display = ['name', 'host']
     search_fields = ['name']
     list_filter = ['topic', 'host']
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    search_fields = ['name']
